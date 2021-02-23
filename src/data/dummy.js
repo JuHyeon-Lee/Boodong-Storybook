87535cfe26f1f85717bd6081039af37ca02b7552
JuHyeon-Lee <leon6095@gmail.com>
2021-02-24 03:46:15 +0900

    chore: dummy 데이터 추가

---
+++ src/data/dummy.js
@@ -0,0 +1,28 @@
+export const sales = [
+    {
+        "saleId": 1,
+        "registeredDate": "2018-03-01T17:03:50.445428",
+        "saleStatus": "SALE",
+        "name": "부자아파트",
+        "dong": 105,
+        "hosu": 304,
+        "pyung": 32,
+        "direction": "남",
+        "type": "JEONSE",
+        "deposit": null,
+        "rent": 12
+    },
+    {
+        "saleId": 2,
+        "registeredDate": "2018-03-01T17:03:50.445428",
+        "saleStatus": "SALE",
+        "name": "최고부자아파트",
+        "dong": 101,
+        "hosu": 203,
+        "pyung": 54,
+        "direction": "동",
+        "type": "TRADE",
+        "deposit": null,
+        "rent": 11
+    }
+];
