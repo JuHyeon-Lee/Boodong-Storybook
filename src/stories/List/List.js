c2c279bbd8c4a7fd9912de7e92dfa9d9e1c5a8e4
JuHyeon-Lee <leon6095@gmail.com>
2021-03-06 21:26:00 +0900

    feat: 판매중, 판매완료, 보류중 필터 추가

--- src/stories/List/List.js
+++ src/stories/List/List.js
@@ -7,10 +7,17 @@ import { ListItem } from '../ListItem/ListItem';
 
 export const List = ({ sales, type, status}) => (
     <div class='sales-list'>
-        {type==='TRADE' ? <label class='selected-tab'>매매</label> : <label class='unselected-tab'>매매</label>}
-        {type==='JEONSE' ? <label class='selected-tab'>전세</label> : <label class='unselected-tab'>전세</label>}
-        {type==='WALLSE' ? <label class='selected-tab'>월세</label> : <label class='unselected-tab'>월세</label>}
-        {sales.map(element => element.type===type ? <ListItem salesItem={element}/> : <></>)}
+        <div class='sales-list-tab'>
+            {type==='TRADE' ? <label class='selected-tab'>매매</label> : <label class='unselected-tab'>매매</label>}
+            {type==='JEONSE' ? <label class='selected-tab'>전세</label> : <label class='unselected-tab'>전세</label>}
+            {type==='WALLSE' ? <label class='selected-tab'>월세</label> : <label class='unselected-tab'>월세</label>}
+        </div>
+        <div class='sales-list-nav'>
+            {status==='SALE' ? <label class='selected-nav'>판매중</label> : <label class='unselected-nav'>판매중</label>}
+            {status==='SOLDOUT' ? <label class='selected-nav'>판매완료</label> : <label class='unselected-nav'>판매완료</label>}
+            {status==='HOLDING' ? <label class='selected-nav'>보류중</label> : <label class='unselected-nav'>보류중</label>}
+        </div>
+        {sales.map(element => element.type===type && element.saleStatus===status ? <ListItem salesItem={element}/> : <></>)}
     </div>
 );
 
